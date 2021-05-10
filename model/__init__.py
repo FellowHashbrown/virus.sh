d0c720dfce887c7a06257a9f36163d649a0a8add
Jonah Pierce <fellowhashbrown@gmail.com>
2021-05-10 01:17:53 -0400

    Imported all subpackage files

---
+++ model/__init__.py
@@ -0,0 +1,9 @@
+from model.abstract import Serializable
+
+from model.error import InvalidNameError
+
+from model.file import Entry
+from model.file import NormalFile
+from model.file import VirusFile
+from model.file import Trash
+from model.file import Directory
