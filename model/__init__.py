242eea349cb6ea9695c3a4bc1750dd2d4a787013
Jonah Pierce <fellowhashbrown@gmail.com>
2021-05-11 14:27:43 -0400

    Removed Trash class in favor of just using Directory

--- model/__init__.py
+++ model/__init__.py
@@ -2,4 +2,3 @@ from model.entry import Entry
 from model.normal_file import NormalFile
 from model.virus_file import VirusFile
 from model.directory import Directory
-from model.trash import Trash
--- model/trash.py
+++
@@ -1,5 +0,0 @@
-from model import Directory
-
-
-class Trash(Directory):
-    pass
