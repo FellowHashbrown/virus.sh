a61f5d6adcf166610fa1dd1e304aa683f041558d
Jonah Pierce <fellowhashbrown@gmail.com>
2021-05-11 23:04:09 -0400

    Included SaveFile import in model package

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
 from model.entry import Entry
 from model.normal_file import NormalFile
 from model.virus_file import VirusFile
+from model.save_file import SaveFile
 from model.directory import Directory
