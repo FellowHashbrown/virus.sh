a7b488156db7a4b17d2f7be6a7dbb738b76df7c0
Jonah Pierce <fellowhashbrown@gmail.com>
2021-05-12 12:09:33 -0400

    Implemented virus generation feature

--- model/util/filesystem.py
+++ model/util/filesystem.py
@@ -1,7 +1,7 @@
 from random import choice, randint
 from typing import Tuple
 
-from model import Directory, NormalFile
+from model import Directory, NormalFile, VirusFile
 
 valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-"
 valid_exts = [".py", ".sh", ".png", ".jpeg", ".jpg", ".ico", ".c", ".dat", ".db",
@@ -9,6 +9,12 @@ valid_exts = [".py", ".sh", ".png", ".jpeg", ".jpg", ".ico", ".c", ".dat", ".db"
               ".js", ".json", ".lisp", ".m", ".md", ".mpg", ".o", ".p", ".pak",
               ".7z", ".zip", ".pem", ".pkl", ".r", ".svg", ".tar", ".ttf", ".x",
               ".xar", ".yaml", ".yml"]
+valid_virus_exts = [".py", ".sh", ".c", ".jar", ".js", ".lisp"]
+
+
+def generate_filename() -> str:
+    """Returns a randomly generated filename with a random extension"""
+    return "".join([choice(valid_chars) for _ in range(randint(5, 20))]) + choice(valid_exts)
 
 
 def generate_filesystem(username: str) -> Tuple[Directory, int]:
@@ -23,10 +29,29 @@ def generate_filesystem(username: str) -> Tuple[Directory, int]:
 
     file_count = 0
     for subdir in range(10):
-        tmpdir, amt_files = generate_directory(root)
+        tmpdir, amt_files = generate_directory(user_dir)
         root.add_entry(tmpdir)
         file_count += amt_files
     return root, file_count
+
+
+def generate_virus(root_directory: Directory, n: int = 1):
+    """Randomly places virus files throughout the system
+    starting at the directory specified
+
+    :param root_directory: The directory to start placing the virus files at
+    :param n: The amount of virus files to place
+    """
+    for virus in range(n):
+        choose_dir = randint(0, 100) % 2 == 0
+        filename = generate_filename()
+        if choose_dir:  # Choose a directory to move into to place the file
+            target = choice(root_directory.get_entries())
+            while not isinstance(target, Directory):
+                target = choice(root_directory.get_entries())
+            generate_virus(target)  # Recursively call this but only place 1
+        else:  # Place the file in the current directory
+            root_directory.add_entry(VirusFile(virus + 1, filename, root_directory))
 
 
 def generate_directory(parent: Directory, depth: int = 0) -> Tuple[Directory, int]:
@@ -51,9 +76,9 @@ def generate_directory(parent: Directory, depth: int = 0) -> Tuple[Directory, in
     used_filenames = []
     for i in range(randint(2, 20)):
         total_files += 1
-        filename = "".join([choice(valid_chars) for _ in range(randint(5, 20))]) + choice(valid_exts)
+        filename = generate_filename()
         while filename in used_filenames:
-            filename = "".join([choice(valid_chars) for _ in range(randint(5, 20))]) + choice(valid_exts)
+            filename = generate_filename()
         used_filenames.append(filename)
         directory.add_entry(NormalFile(filename, directory))
 
