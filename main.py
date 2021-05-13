4c302a8e196d8b8deb4c05917b9a3c4a53817647
Jonah Pierce <fellowhashbrown@gmail.com>
2021-05-12 20:46:53 -0600

    Refined the fake console just a bit more; Autocomplete, left and right arrows now work better

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Text, END
+from tkinter import Tk, Text, END, BOTH
 from model.console import Console
 
 
@@ -16,18 +16,82 @@ class ConsoleUI(Tk):
 
         self.__text = Text(self)
         self.__text.insert("end", f"{self.__console.get_prompt()}")
-        self.__text.configure(font=("Courier New", 15), bg="black", fg="white")
-        self.__text.pack()
+        self.__text.configure(font=("Courier New", 15), bg="black", fg="white",
+                              insertbackground="white", insertwidth=4)
+        self.__text.pack(expand=True, fill=BOTH)
 
+        self.__text.bind("<Left>", self.on_left_arrow)
+        self.__text.bind("<Right>", self.on_right_arrow)
         self.__text.bind("<Return>", self.on_enter)
         self.__text.bind("<BackSpace>", self.on_bs)
         self.__text.bind("<KeyPress>", self.on_key_press)
+        self.__text.bind("<Tab>", self.on_tab)
 
         self.__current_line = ""
+        self.__current_index = 0
+
+    def on_left_arrow(self, _):
+        if self.__current_index > 0:
+            self.__current_index -= 1
+            if self.__current_index < 0:
+                self.__current_index = 0
+                return "break"
+        else:
+            return "break"
+
+    def on_right_arrow(self, _):
+        if self.__current_index <= len(self.__current_line):
+            self.__current_index += 1
+        else:
+            return "break"
+
+    def on_tab(self, _):
+        """Whenever the tab key is pressed, try auto-completing the text"""
+
+        # Try splitting the current line by spaces in order to get the current
+        # line and split the possible directory by using the "/" as a separator
+        line_split = self.__current_line.split(" ")
+        no_spaces = len(line_split) == 1
+        if no_spaces:
+            dir_line_split = self.__current_line.split("/")
+        else:
+            dir_line_split = line_split[-1].split("/")
+
+        # Iterate through the entries in the most recent directory to try to auto complete it
+        if len(dir_line_split) == 1:
+            current_dir = self.__console.get_current_dir()
+        else:
+            current_dir = self.__console.get_current_dir().get_entry(dir_line_split[-2])
+
+        # If the directory exists, try finding the entry that matches the last result
+        if current_dir and dir_line_split[-1]:
+            for entry in current_dir.get_entries():
+                if entry.get_name().startswith(dir_line_split[-1]):
+
+                    # Find the text that needs to be appended by only adding the
+                    #   text that is left in the found entry
+                    # Update the last index of the dir_line_split, join it together using "/"
+                    #   and replace the current line with the updated string
+                    appended_text = entry.get_name()[len(dir_line_split[-1]):]
+                    dir_line_split[-1] = entry.get_name()
+                    line_split[-1] = "/".join(dir_line_split)
+                    if no_spaces:
+                        self.__current_line = "/".join(line_split)
+                    else:
+                        self.__current_line = " ".join(line_split)
+                    self.__current_index = len(self.__current_line)
+                    self.__text.insert("end", appended_text)
+        return "break"
 
     def on_key_press(self, event):
         """Whenever a keyboard key is pressed"""
-        self.__current_line += event.char
+        if len(self.__current_line) != self.__current_index:
+            line_list = list(self.__current_line)
+            line_list.insert(self.__current_index, event.char)
+            self.__current_line = "".join(line_list)
+        else:
+            self.__current_line += event.char
+        self.__current_index += 1
 
     def on_enter(self, _):
         """Whenever the enter/return key is pressed"""
@@ -37,8 +101,11 @@ class ConsoleUI(Tk):
         if result:
             if result == "@clear":
                 self.__text.delete(1.0, "end")
+            elif result == "@main_menu":
+                self.__console.main_menu()
+                self.__text.delete(1.0, "end")
+                cleared = True
             elif result == "@exit":
-                self.__text.insert("end", "\nbye!")
                 exit(0)
             else:
                 if result.startswith("@prompt:"):
@@ -50,13 +117,19 @@ class ConsoleUI(Tk):
         if not prompted:
             self.__text.insert("end", f"{self.__console.get_prompt()}")
         self.__current_line = ""
+        self.__current_index = 0
         self.__text.see(END)
+        self.__text.mark_set("insert", END)
         return "break"
 
     def on_bs(self, _):
         """Whenever the backspace key is pressed"""
-        if len(self.__current_line) > 0:
-            self.__current_line = self.__current_line[:-1]
+        if self.__current_index > 0 and len(self.__current_line) > 0:
+            self.__current_index -= 1
+            if self.__current_index == len(self.__current_line):
+                self.__current_line = self.__current_line[:-1]
+            else:
+                self.__current_line = self.__current_line[:self.__current_index] + self.__current_line[self.__current_index + 1:]
             self.__text.delete("insert -1 chars", "insert")
         return "break"
 
