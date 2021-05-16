d2adce42a93e334329131700087fcd568c6019e8
Jonah Pierce <fellowhashbrown@gmail.com>
2021-05-16 17:30:32 -0600

    Updated util package import statements

--- model/util/__init__.py
+++ model/util/__init__.py
@@ -1,5 +1,6 @@
 from .hexable import Hexable
-from .filesystem import generate_filesystem
+from .filesystem import generate_filesystem, choose_random_file, choose_random_directory, generate_virus
 from .save import Save
 from .options import Options
 from .command import ls, cd, cat, rm, chunks, track, trace, mntr
+from .virus import Virus
