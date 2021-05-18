a2d56da4da17602122daa49e4e05e54e1f0d591f
Jonah Pierce <fellowhashbrown@gmail.com>
2021-05-17 22:07:57 -0600

    Implemented set_theme.sh

--- script/options/__init__.py
+++ script/options/__init__.py
@@ -1,1 +1,2 @@
 from script.options.change_prompt_char import change_prompt_char
+from script.options.set_theme import set_theme
---
+++ script/options/set_theme.py
@@ -0,0 +1,22 @@
+from model.util import ls
+
+
+def set_theme(console, command):
+    """Controls the logic surrounding the set_theme.sh fakescript
+    in the game
+
+    :param console: The console object that is responsible for the fake console
+    :param command: The command that has been entered
+    """
+    if command.split("/")[-1] == "set_theme.sh":
+        console["on_set_theme"] = True
+        result = ls(console, [str(console.get_current_dir().get_entry("themes"))])
+        return f"@prompt:{result}\nEnter a theme name: "
+
+    elif console["on_set_theme"]:
+        console["on_set_theme"] = False
+        for theme, _ in console.get_themes():
+            if theme.get_name() == command:
+                console.set_current_theme(command)
+                return f"{theme.get_name()} set as new theme"
+        return f"{command} is not a valid theme"
