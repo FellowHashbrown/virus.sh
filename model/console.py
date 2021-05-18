7e7e6064bddd6d0deb883474b88e5694c06eba77
Jonah Pierce <fellowhashbrown@gmail.com>
2021-05-17 22:20:40 -0600

    Implemented smoother transitions between main menu scripts and other commands

--- model/console.py
+++ model/console.py
@@ -5,6 +5,8 @@ from model.error import InvalidNameError
 from model.theme import Theme, Element
 from model.util import *
 
+from script import run_script
+
 
 class Console:
     """The Console class is what keeps track of the current save, the current directory
@@ -14,33 +16,72 @@ class Console:
     VIRUS_WEIGHT = 0.7
     NORMAL_WEIGHT = 0.3
 
-    def __init__(self):
+    def __init__(self, console_ui):
+        self.__console_ui = console_ui
         self.__save = None
         self.__in_play = False
         self.__current_dir = None
         self.__previous_dir = None
-        self.__tracked_files = {}
 
+        # Main Menu Script states
         self.__on_new_game = False
         self.__on_load_game = False
+        self.__on_delete_game = False
+
         self.__on_prompt_char = False
+        self.__on_set_theme = False
 
         self.__on_new_theme = False
         self.__on_edit_theme = False
         self.__on_delete_theme = False
 
+        # Game variables
         self.__virus = None
         self.__saves = []
-        self.__themes = {
-            "obsidian": Theme("obsidian",
-                              game = Element("black", "#EC7600"),
-                              menu = Element("black", "#678CB1"),
-                              curdir = Element("black", "#A0A2A4"),
-                              directory = Element("black", "#A082BD"),
-                              file = Element("black", "#FFCD22"))
-        }
+        self.__themes = []
+        self.__current_theme = None
 
         self.main_menu()
+
+    def __getitem__(self, key: str):
+        if key == "on_new_game":
+            return self.__on_new_game
+        elif key == "on_load_game":
+            return self.__on_load_game
+        elif key == "on_delete_game":
+            return self.__on_delete_game
+
+        elif key == "on_prompt_char":
+            return self.__on_prompt_char
+        elif key == "on_set_theme":
+            return self.__on_set_theme
+
+        elif key == "on_new_theme":
+            return self.__on_new_theme
+        elif key == "on_edit_theme":
+            return self.__on_edit_theme
+        elif key == "on_delete_theme":
+            return self.__on_delete_theme
+
+    def __setitem__(self, key: str, value: bool):
+        if key == "on_new_game":
+            self.__on_new_game = value
+        elif key == "on_load_game":
+            self.__on_load_game = value
+        elif key == "on_delete_game":
+            self.__on_delete_game = value
+
+        elif key == "on_prompt_char":
+            self.__on_prompt_char = value
+        elif key == "on_set_theme":
+            self.__on_set_theme = value
+
+        elif key == "on_new_theme":
+            self.__on_new_theme = value
+        elif key == "on_edit_theme":
+            self.__on_edit_theme = value
+        elif key == "on_delete_theme":
+            self.__on_delete_theme = value
 
     # # # # # # # # # # # # # # # # # # # #
 
@@ -66,13 +107,29 @@ class Console:
 
     # # # # # # # # # # # # # # # # # # # #
 
+    def get_saves(self) -> List[Save]:
+        """Returns the list of saves that exist in the game"""
+        return list(self.__saves)
+
     def get_save(self) -> Save:
         """Returns the current save being used by the console"""
         return self.__save
 
+    def get_root(self) -> Directory:
+        """Returns the very root of the filesystem"""
+        return self.__save.get_root()
+
     def get_trash(self) -> Directory:
         """Returns the Trash directory"""
         return self.__save.get_trash()
+
+    def remove_save(self, username: str):
+        """Removes the save with the specified username from the Saves list"""
+        for i in range(len(self.__saves)):
+            if self.__saves[i][0].get_username() == username:
+                self.__saves.pop(i)
+                self.__current_dir.get_entry("gameSaves").remove_entry(username)
+                break
 
     def get_current_dir(self) -> Directory:
         """Returns the current directory"""
@@ -81,10 +138,6 @@ class Console:
     def get_previous_dir(self) -> Directory:
         """Returns the previous directory which is what is used when going back from the Trash"""
         return self.__previous_dir
-
-    def get_root(self) -> Directory:
-        """Returns the very root of the filesystem"""
-        return self.__save.get_root()
 
     def get_prompt(self) -> str:
         """Returns the command line prompt in the console"""
@@ -103,6 +156,10 @@ class Console:
         """Returns whether or not a game is currently being played"""
         return self.__in_play
 
+    def in_play(self):
+        """Sets the game as being currently played"""
+        self.__in_play = True
+
     # # # # # # # # # # # # # # # # # # # #
 
     def parse(self, cmd: str) -> Optional[str]:
@@ -110,73 +167,8 @@ class Console:
         cmd = cmd.split(" ")
         cmd, args = cmd[0], cmd[1:]
 
-        # Check if a "script" was run from one of the main menu folders
-        if cmd.startswith("./") and cmd.endswith(".sh") and not self.__in_play:
-            if cmd.split("/")[-1] == "new_game.sh":
-                self.__on_new_game = True
-                return "@prompt:Enter a username: "
-            elif cmd.split("/")[-1] == "load_game.sh":
-                self.__on_load_game = True
-                return f"@prompt:{ls(self, [str(self.get_current_dir().get_entry('gameSaves'))])}\nEnter the username: "
-            elif cmd.split("/")[-1] == "change_prompt_character.sh":
-                self.__on_prompt_char = True
-                return "@prompt:Enter the new prompt character: "
-            elif cmd.split("/")[-1] == "new_theme.sh":
-                self.__on_new_theme = True
-                return "@prompt:Enter the name of the theme: "
-            elif cmd.split("/")[-1] == "edit_theme.sh":
-                self.__on_edit_theme = True
-                return "@prompt:Enter the name of the theme: "
-            elif cmd.split("/")[-1] == "delete_theme.sh":
-                self.__on_delete_theme = True
-                return "@prompt:Enter the name of the theme: "
-
-        # Check the callback from the new game script
-        elif self.__on_new_game:
-            self.__on_new_game = False
-            for gamesave, _ in self.__saves:
-                if gamesave.get_username() == cmd:
-                    return f"{gamesave.get_username()} already exists!"
-            try:
-                self.set_save(cmd)
-                self.__in_play = True
-                return f"Created gamesave {self.get_save().get_username()} ..."
-            except InvalidNameError as e:
-                return str(e)
-
-        # Check the callback from the load game script
-        elif self.__on_load_game:
-            self.__on_load_game = False
-            for gamesave, _ in self.__saves:
-                if gamesave.get_username() == cmd:
-                    gamesave.generate()
-                    self.set_save(gamesave.get_username())
-                    self.__in_play = True
-                    return f"Loaded gamesave {gamesave.get_username()} ..."
-            return f"No gamesave found for {cmd}"
-
-        # Check the callback from the prompt character script
-        elif self.__on_prompt_char:
-            if len(cmd) != 1:
-                return "@prompt:The character must be only 1 character\nEnter the new prompt character: "
-            Options.get_instance().set_prompt_char(cmd)
-            self.__on_prompt_char = False
-            return None
-
-        # Check the callback from the new theme script
-        elif self.__on_new_theme:
-            pass
-
-        # Check the callback from the edit theme script
-        elif self.__on_edit_theme:
-            pass
-
-        # Check the callback from the delete theme script
-        elif self.__on_delete_theme:
-            pass
-
         # Run the commands like normal
-        elif cmd == "clear":
+        if cmd == "clear":
             return "@clear"
         elif cmd == "ls":
             return ls(self, args)
@@ -201,6 +193,10 @@ class Console:
                 return "@main_menu"
             return "@exit"
 
+        # Check if a "script" was run from one of the main menu folders
+        elif not self.is_in_play():
+            return run_script(self, cmd)
+
     # # # # # # # # # # # # # # # # # # # #
 
     def game_over(self):
@@ -210,20 +206,40 @@ class Console:
         self.__in_play = False
         return "@game_over"
 
-    def get_themes(self) -> dict:
+    def get_themes(self) -> list:
         """Loads the themes from the theme file into the console"""
         return self.__themes
 
-    def main_menu(self):
-        """Generates the main menu mini filesystem when starting up the game"""
+    def get_current_theme(self) -> str:
+        """Returns the name of the theme that is currently set"""
+        return self.__current_theme
 
-        # Set the main_menu current directory
-        self.__current_dir = Directory("main_menu")
-        if self.__virus:
-            self.__virus.stop()
-            self.__virus = None
+    def set_current_theme(self, theme: str):
+        """Sets the current console theme to the one specified"""
+        self.__current_theme = theme
 
-        # Load the list of saves from the save directory
+    def add_theme(self, theme: Theme):
+        """Adds a new theme to the theme list"""
+        theme_str = ("name: {}\n" +
+                     "\tmain (bg/fg): {}/{}\n" +
+                     "\tgame (bg/fg): {}/{}\n" +
+                     "\tmenu (bg/fg): {}/{}\n" +
+                     "\tcurrent directory (bg/fg): {}/{}\n" +
+                     "\tdirectories (bg/fg): {}/{}\n" +
+                     "\tfiles (bg/fg): {}/{}").format(
+            theme.get_name(),
+            theme["main"]["bg"], theme["main"]["fg"],
+            theme["game"]["bg"], theme["game"]["fg"],
+            theme["menu"]["bg"], theme["menu"]["fg"],
+            theme["curdir"]["bg"], theme["curdir"]["fg"],
+            theme["directory"]["bg"], theme["directory"]["fg"],
+            theme["file"]["bg"], theme["file"]["fg"])
+        theme = (theme, theme_str)
+        self.__themes.append(theme)
+        self.__console_ui.add_theme(theme[0])
+
+    def load_saves(self):
+        """Loads the saves from the Save directory"""
         self.__saves: List[Union[Tuple[Save, str], Save]] = Save.load_saves()
         for i in range(len(self.__saves)):
             v_d, v_t = self.__saves[i].get_virus_files()  # Deleted virus files, Total virus files
@@ -241,23 +257,62 @@ class Console:
                     Console.NORMAL_WEIGHT * (n_r / (n_d if n_d != 0 else 1)) * 100, 2))
             self.__saves[i] = (self.__saves[i], save_str)
 
+    def load_themes(self):
+        """Loads the themes from the Theme directory"""
+        self.__themes: List[Union[Tuple[Theme, str], Theme]] = Theme.load_themes()
+        for i in range(len(self.__themes)):
+            theme_str = ("name: {}\n" +
+                         "\tmain (bg/fg): {}/{}\n" +
+                         "\tgame (bg/fg): {}/{}\n" +
+                         "\tmenu (bg/fg): {}/{}\n" +
+                         "\tcurrent directory (bg/fg): {}/{}\n" +
+                         "\tdirectories (bg/fg): {}/{}\n" +
+                         "\tfiles (bg/fg): {}/{}").format(
+                self.__themes[i].get_name(),
+                self.__themes[i]["main"]["bg"], self.__themes[i]["main"]["fg"],
+                self.__themes[i]["game"]["bg"], self.__themes[i]["game"]["fg"],
+                self.__themes[i]["menu"]["bg"], self.__themes[i]["menu"]["fg"],
+                self.__themes[i]["curdir"]["bg"], self.__themes[i]["curdir"]["fg"],
+                self.__themes[i]["directory"]["bg"], self.__themes[i]["directory"]["fg"],
+                self.__themes[i]["file"]["bg"], self.__themes[i]["file"]["fg"])
+            self.__themes[i] = (self.__themes[i], theme_str)
+
+    def main_menu(self):
+        """Generates the main menu mini filesystem when starting up the game"""
+
+        # Set the main_menu current directory
+        self.__current_dir = Directory("main_menu")
+        if self.__virus:
+            self.__virus.stop()
+            self.__virus = None
+
+        self.load_saves()
+        self.load_themes()
+
         # Add the play directory
         play_dir = Directory("play", parent=self.__current_dir)
         play_new = NormalFile("new_game.sh", parent=play_dir)
         play_load = NormalFile("load_game.sh", parent=play_dir)
-        play_dir.add_entries(play_new, play_load)
+        play_delete = NormalFile("delete_game.sh", parent=play_dir)
+        play_dir.add_entries(play_new, play_load, play_delete)
 
         # Add the game saves directory, pulling from the saves loaded from above
         game_saves = Directory("gameSaves", parent=self.__current_dir)
         for save_obj in self.__saves:
             game_saves.add_entry(SaveFile(save_obj[0].get_username(), save_obj[1], game_saves))
 
-        # Add the options directory
+        # Add the options directory / theme scripts
         options_dir = Directory("options", parent=self.__current_dir)
         options_char = NormalFile("change_prompt_character.sh", parent=options_dir)
-        options_dir.add_entries(options_char)
+        theme_new = NormalFile("new_theme.sh", parent=options_dir)
+        theme_edit = NormalFile("edit_theme.sh", parent=options_dir)
+        theme_delete = NormalFile("delete_theme.sh", parent=options_dir)
+        theme_set = NormalFile("set_theme.sh", parent=options_dir)
+        options_dir.add_entries(options_char, theme_new, theme_edit, theme_delete, theme_set)
 
-        self.__current_dir.add_entries(play_dir, game_saves, options_dir)
+        # Add the themes directory, pulling from the themes loaded from above
+        themes = Directory("themes", parent=self.__current_dir)
+        for theme_obj in self.__themes:
+            themes.add_entry(SaveFile(theme_obj[0].get_name(), theme_obj[1], themes))
 
-    def theme_config(self):
-        pass
+        self.__current_dir.add_entries(play_dir, game_saves, options_dir, themes)
